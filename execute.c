ae94cc660fe5b518af49275f4a7b91f0defc1d8c
Freddiewrig <thecollegeace1@gmail.com>
2023-10-20 05:56:17 -0700

    simple_shell

--- execute.c
+++ execute.c
@@ -6,19 +6,13 @@
 */
 char *find_path(void)
 {
-	int x;
 	char **env = environ, *path = NULL;
 
 	while (*env)
 	{
 		if (_strncmp(*env, "PATH=", 5) == 0)
 		{
-			path = *env;
-			while (*path && x < 5)
-			{
-				path++;
-				x++;
-			}
+			path = *env + 5;
 			return (path);
 		}
 		env++;
